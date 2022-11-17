168e5becbd1fd79fc97a62a249681dc8bce7edd0
Yormi Altamiranda <yormian@gmail.com>
2022-11-16 19:37:14 -0500

    :arrow_up: Cambios

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
+const BundleAnalyzerPublin = require('webpack-bundle-analyzer');
+const { sources } = require("webpack");
 
 
 
@@ -14,7 +16,7 @@ module.exports = {
 		assetModuleFilename: './assets/images/[hash][ext][query]'
     },
 	mode: 'development',
-	watch: true,
+	devtool: 'source-map',
     resolve: {
         extensions: ['.js'],
 		alias: {
@@ -80,5 +82,15 @@ module.exports = {
 			]
 		}),
 		new DotEnv(),
+		new BundleAnalyzerPublin(),
     ],
+
+	devServer: {
+		static:{ 
+			directory: path.join(__dirname, 'dist')
+		},
+		compress: true,
+		historyApiFallback: true,
+		port: 3006,
+	  },
 }
